157ddb6e825b3cf6770cf2e01cee8f5b944f4ffc
mimrpq888 <mimrpq@gmail.com>
2025-07-28 16:33:41 +0600

    modified component functionalities and integrated AxiosSecure interceptor for secure API calls

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,20 +1,28 @@
 
-import axios from 'axios';
 import React, { useContext } from 'react';
+
+import axios from 'axios';
 import { AuthContext } from '../Provider/AuthContext';
 
-const useAxiosSecure = () => {
-const {user}=  useContext(AuthContext)
-
-// console.log(user.accessToken);
 const axiosSecure = axios.create({
-    baseURL:"http://localhost:5000",
-    headers:{
-        Authorization:`Bearer ${user.accessToken}`
-    }
-})
+    baseURL: `https://unit-sphere-server.vercel.app`
+});
+
+const useAxiosSecure = () => {
+    const { user } = useContext(AuthContext)
+
+    // console.log(user.accessToken);
+
+
+    axiosSecure.interceptors.request.use(config => {
+        config.headers.Authorization = `Bearer ${user.accessToken}`
+        return config
+    }, error => {
+        return Promise.reject(error)
+    })
+
     return axiosSecure
-    
+
 };
 
 export default useAxiosSecure;
--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -1,20 +1,59 @@
-import React, { useEffect, useState } from 'react';
+
+import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from './useAxiosSecure';
+import { useContext } from 'react';
+import { AuthContext } from '../Provider/AuthContext';
 
 
 const useRole = () => {
-    const [role, setRole] = useState('')
-    const  axiosSecure = useAxiosSecure();
-    const[loading, setLoading] = useState(true)
+    const axiosSecure = useAxiosSecure();
+    const {user, userLoading}=  useContext(AuthContext)
 
-    useEffect(()=>{
-        axiosSecure("/user-role").then((res)=>{
-            setRole(res.data.role)
-            setLoading(false)
-        })
-    },[])
+    const {
+        data: roleData,
+        isLoading,
+        refetch,
+    } = useQuery({
+        queryKey: ['userRole'],
+        enabled: !userLoading && !!user?.email,
+        queryFn: async () => {
+            const res = await axiosSecure.get('/user-role');
+            return res.data;
+        },
+    });
 
-    return {role, loading};
+    return {
+        role: roleData?.role || '',
+        loading: isLoading,
+        refetch,
+    };
 };
 
 export default useRole;
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import useAxiosSecure from './useAxiosSecure';
+
+
+// const useRole = () => {
+//     const [role, setRole] = useState('')
+//     const  axiosSecure = useAxiosSecure();
+//     const[loading, setLoading] = useState(true)
+
+//     useEffect(()=>{
+//         axiosSecure("/user-role").then((res)=>{
+//             setRole(res.data.role)
+//             setLoading(false)
+//         })
+//     },[])
+
+//     return {role, loading};
+// };
+
+// export default useRole;
+
+
+
